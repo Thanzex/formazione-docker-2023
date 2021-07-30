a2e46482831e410fd41d8136530bee4923c521a1
Francesco Bruno <b.frenc@hotmail.it>
2021-07-31 00:29:19 +0200

    Volumes and environment demo

--- SimpleApp/index.js
+++ SimpleApp/index.js
@@ -3,11 +3,31 @@ const express = require('express')
 const app = express()
 const port = 3030
 
+console.log(process.env)
+app.use(logger())
+
 app.use('/', express.static('public'))
 
-app.get('/alive', (req, res) => {
+app.get('/health', (req, res) => {
   res.status(200)
   res.send('Ok')
 })
 
+app.get('/message',(req,res) => {
+  if (!process.env.MESSAGE)
+    res.send('No Message.')
+  else 
+    res.send(process.env.MESSAGE)
+
+
+})
+
 app.listen(port)
+
+function logger() {
+  return (req, res, next) => {
+    console.log(`${req.method} : ${req.url}`)
+    next()
+  }
+}
+
